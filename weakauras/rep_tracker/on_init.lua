abca04764bfdcf5c865ff05f7854cae2203d076e
Pewtro <29204244+Pewtro@users.noreply.github.com>
2025-03-05 11:48:23 +0100

    [Reputation] Pull in current version

--- weakauras/rep_tracker/on_init.lua
+++ weakauras/rep_tracker/on_init.lua
@@ -2,7 +2,17 @@ aura_env.reputations = {
     -- ["factionID"] = iconID,
     -- factionID is used as the key in custom options
     -- iconID is the ID of the icon you want shown for the reputation, if you don't define one, the fallback icon will be used
-
+    
+    --The War Within
+    ["2600"] = 5891370, -- The Severed Threads
+    ["2605"] = 5862762, -- The General
+    ["2607"] = 5862763, -- The Vizier
+    ["2601"] = 5862764, -- The Weaver
+    ["2570"] = 5891368, -- Hallowfall Arathi
+    ["2594"] = 5891367, -- Assembly of the Deeps
+    ["2590"] = 5891369, -- Council of Dornogal
+    ["2640"] = 5453546, -- Brann Bronzebeard
+    
     -- Dragonflight
     ["2503"] = 4566591, -- Maruuk Centaur
     ["2507"] = 3012071, -- Dragonscale Expedition
@@ -11,6 +21,8 @@ aura_env.reputations = {
     ["2523"] = 4574311, -- Dark Talons
     ["2524"] = 4574311, -- Obsidian Warders
     ["2564"] = 1786409, -- Loamm Niffen
+    ["2574"] = 341763, -- Dream Wardens
+    
     -- Shadowlands
     ["2413"] = 3257751, -- Court of Harvesters
     ["2407"] = 3257748, -- The Ascended
@@ -21,6 +33,7 @@ aura_env.reputations = {
     ["2432"] = 3528299, -- Ve'nari
     ["2472"] = 2101967, -- The Archivists' Codex
     ["2478"] = 3601566, -- The Enlightened
+    
     -- Battle for Azeroth
     ["2164"] = 132275, -- Champions of Azeroth
     ["2161"] = 2065572, -- Order of Embers
@@ -37,6 +50,7 @@ aura_env.reputations = {
     ["2391"] = 3208032, -- Rustbolt Resistance
     ["2373"] = 2620776, -- The Unshackled
     ["2400"] = 2620775, -- Waveblade Ankoan
+    
     -- Legion
     ["1859"] = 1525362, -- The nightfallen
     ["2018"] = 1531511, -- Talon's Vengeance
@@ -48,6 +62,7 @@ aura_env.reputations = {
     ["2045"] = 1585245, -- Armies of Legionfall
     ["2165"] = 1608729, -- Army of the Light
     ["1894"] = 1447613, -- The Wardens
+    
     -- Warlords of Draenor
     ["1711"] = 1052654, -- Steamwheedle Preservation Society
     ["1445"] = 1044164, -- Frostwolf Orcs
@@ -59,12 +74,25 @@ aura_env.reputations = {
     ["1731"] = 1048727, -- Council of Exarchs
     ["1848"] = 133284, -- Vol'jin's Headhunters
     ["1847"] = 921904, -- Hand of the Prophet
+    
     -- Mists of Pandaria
     ["1271"] = 646324, -- Order of the Cloud Serpent
     ["1341"] = 656174, -- The August Celestials
     ["1270"] = 645204, -- Shado-Pan
     ["1269"] = 643910, -- Golden Lotus
     ["1272"] = 645198, -- The Tillers
+    ["1277"] = 134141, -- Chee Chee
+    ["1275"] = 454045, -- Ella
+    ["1283"] = 651600, -- Farmer Fung
+    ["1282"] = 651598, -- Fish Fellreed
+    ["1281"] = 651596, -- Gina Mudclaw
+    ["1279"] = 651597, -- Haohan Mudclaw
+    ["1273"] = 651591, -- Jogu the Drunk
+    ["1276"] = 651567, -- Old Hillpaw
+    ["1278"] = 133975, -- Sho
+    ["1280"] = 651569, -- Tina Mudclaw
+    ["1302"] = 4620674, -- The Anglers
+    ["1358"] = 4620674, -- Nat Pagle
     ["1435"] = 838811, -- Shado-Pan Assault
     ["1492"] = 901300, -- Emperor Shaohao
     ["1345"] = 645218, -- The Lorewalkers
@@ -74,6 +102,7 @@ aura_env.reputations = {
     ["1337"] = 646377, -- The Klaxxi
     ["1387"] = 801132, -- Kirin Tor Offensive
     ["1388"] = 838819, -- Sunreaver Onslaught
+    
     -- Cataclysm
     ["1173"] = 456574, -- Ramkahen
     ["1204"] = 512609, -- Avengers of Hyjal
@@ -84,6 +113,7 @@ aura_env.reputations = {
     ["1178"] = 409548, -- Hellscream's Reach
     ["1174"] = 456575, -- Wildhammer Clan
     ["1177"] = 462529, -- Baradin's Wardens
+    
     -- Wrath of the Lich King
     ["1119"] = 136050, -- The Sons of Hodir
     ["1098"] = 133445, -- The Ebon Blade
@@ -104,6 +134,7 @@ aura_env.reputations = {
     ["1064"] = 255142, -- The Taunka
     ["1126"] = 134474, -- The Frostborn
     ["1068"] = 134474, -- Explorers' League
+    
     -- The Burning Crusade
     ["1015"] = 132250, -- Netherwing
     ["978"] = 134060, -- Kurenai
@@ -125,9 +156,23 @@ aura_env.reputations = {
     ["947"] = 134504, -- Thrallmar
     ["967"] = 133404, -- The Violet Eye
     ["1012"] = 133793, -- Ashtongue Deathsworn
-    ["936"] = 135760 -- Shattrath City
-
+    ["936"] = 135760, -- Shattrath City
+    
     -- Vanilla
+}
+
+aura_env.expansions = {
+    "tww",
+    "df",
+    "sl",
+    "bfa",
+    "legion",
+    "wod",
+    "mop",
+    "cataclysm",
+    "wotlk",
+    "tbc",
+    "vanilla"
 }
 
 -- Config
@@ -135,7 +180,7 @@ local colours = aura_env.config.colourOptions
 local general = aura_env.config.general
 
 -- Takes a color picker colour and returns a hex colour usable by WA
-local getColour = function(colour)
+local getColour = function(colour) 
     return CreateColor(unpack(colour)):GenerateHexColor()
 end
 
@@ -144,110 +189,115 @@ local getParagonValue = function(val, threshold, hasReward)
     -- We need to adjust the value since we get the total amount of paragon reputation ever
     -- This will be done by checking if there's a reward and then mathing out the proper amount to show
     local adjustedVal = val
-
-    if val > threshold * 2 or (val > threshold and not hasReward) then
+    
+    if val > (threshold*2) or (val > threshold and not hasReward) then
         adjustedVal = val % threshold
     end
-
+    
     if hasReward then
         adjustedVal = val % threshold + threshold
     end
-
+    
     return adjustedVal
 end
 
 -- Reputation / Renown Name
-aura_env.getColouredRepName = function(repName)
+aura_env.getColouredRepName = function (repName) 
     local repNameColour = getColour(colours.repNameColour)
-    return "|c" .. repNameColour .. repName .. ":|r"
+    return "|c"..repNameColour..repName..":|r"
 end
 
--- Renown (Dragonflight)
-aura_env.getRenownColouredValue = function(
-    currentRenown,
-    renownThreshold,
-    currentRenownLevel,
-    maxRenownLevel,
-    isMaxRenown,
-    factionID)
+
+-- Renown
+aura_env.getRenownColouredValue = function(currentRenown, renownThreshold, currentRenownLevel, maxRenownLevel, isMaxRenown, factionID)
     if (isMaxRenown and factionID) then
         local renownColour = getColour(colours.renownParagonActive)
         local renownParagonAvailable = getColour(colours.renownParagonAvailable)
-
+        
+        
         local curVal, threshold, _, hasRewardPending = C_Reputation.GetFactionParagonInfo(factionID)
-
+        
         if (not curVal or not threshold) then
             return "0"
         end
-
+        
         local uiValue = getParagonValue(curVal, threshold, hasRewardPending)
-
+        
         local colorToShow = hasRewardPending and renownParagonAvailable or renownColour
-
-        return "|c" .. colorToShow .. uiValue .. "/" .. threshold .. "|r"
+        
+        return "|c"..colorToShow..uiValue.."/"..threshold.."|r"    
+        
     end
-
+    
     local showRenownLevel = general.showRenownLevel
     local showRenownProg = general.showRenownProg
-
+    
     local renownColour = getColour(colours.defaultRenown)
-
-    local renownLevelInfo = currentRenownLevel .. "/" .. maxRenownLevel
-    local renownProgressInfo = currentRenown .. "/" .. renownThreshold
-
+    
+    local renownLevelInfo = currentRenownLevel.."/"..maxRenownLevel
+    local renownProgressInfo = currentRenown.."/"..renownThreshold
+    
     if (showRenownLevel and showRenownProg) then
-        return "|c" .. renownColour .. renownProgressInfo .. " (" .. renownLevelInfo .. ")" .. "|r"
+        return "|c"..renownColour..renownProgressInfo.." ("..renownLevelInfo..")".."|r"
     end
-
+    
     if (showRenownLevel) then
-        return "|c" .. renownColour .. renownLevelInfo .. "|r"
+        return "|c"..renownColour..renownLevelInfo.."|r"
     end
-
+    
     if (showRenownProg) then
-        return "|c" .. renownColour .. renownProgressInfo .. "|r"
+        return "|c"..renownColour..renownProgressInfo.."|r"
     end
 end
 
 -- Reputation (Regular)
 aura_env.getColouredValue = function(barMin, barMax, barValue, standingId)
     local nonParagonColour = getColour(colours.nonParagonColour)
-
+    
     -- The UI shows a value/max that is equal to barValue/barMax MINUS barMin
     local currentUIVal = barValue - barMin
     local currentUIMax = barMax - barMin
-
-    local currentStanding =
-        general.showCurrentStandingPreExalted and standingId and
-        " (" .. _G["FACTION_STANDING_LABEL" .. standingId] .. ")" or
-        ""
-
-    return "|c" .. nonParagonColour .. currentUIVal .. "/" .. currentUIMax .. currentStanding .. "|r"
+    
+    if (barValue == barMax and barValue > 0) then
+        currentUIVal = barMax
+        currentUIMax = barMax
+    end
+    
+    local showStanding = general.showCurrentStandingPreExalted and standingId
+    local currentStanding = showStanding and (type(standingId) == number and " (".._G["FACTION_STANDING_LABEL"..standingId]..")" or " ("..standingId..")") or ""
+    
+    
+    return "|c"..nonParagonColour..currentUIVal.."/"..currentUIMax..currentStanding.."|r"
 end
 
--- Paragon
+
+-- Paragon 
 aura_env.getParagonColouredValue = function(val, threshold, hasReward, standingId)
     -- Paragon colours
     local paragonColour = getColour(colours.paragonColour)
     local paragonAvailable = getColour(colours.paragonAvailable)
     local paragonNearCap = getColour(colours.paragonNearCap)
-
+    
     if not (val > threshold) then
-        return "|c" .. paragonColour .. adjustedVal .. "/" .. threshold .. "|r"
+        return "|c"..paragonColour..val.."/"..threshold.."|r"
     end
-
+    
     local adjustedVal = getParagonValue(val, threshold, hasReward)
-
-    local currentStanding =
-        general.showCurrentStandingExalted and standingId and " (" .. _G["FACTION_STANDING_LABEL" .. standingId] .. ")" or
-        ""
-
+    
+    local showStanding = general.showCurrentStandingExalted and standingId
+    local currentStanding = showStanding and (type(standingId) == "number" and " (".._G["FACTION_STANDING_LABEL"..standingId]..")" or " ("..standingId..")") or ""
+    
     if hasReward then
         if adjustedVal < aura_env.config.colourOptions.nearCapThreshold then
-            return "|c" .. paragonAvailable .. adjustedVal .. "/" .. threshold .. currentStanding .. "|r"
+            return "|c"..paragonAvailable..adjustedVal.."/"..threshold..currentStanding.."|r"
         else
-            return "|c" .. paragonNearCap .. adjustedVal .. "/" .. threshold .. currentStanding .. "|r"
+            return "|c"..paragonNearCap..adjustedVal.."/"..threshold..currentStanding.."|r"
+            
         end
     end
-
-    return "|c" .. paragonColour .. adjustedVal .. "/" .. threshold .. currentStanding .. "|r"
+    
+    return "|c"..paragonColour..adjustedVal.."/"..threshold..currentStanding.."|r"
 end
+
+
+
--- weakauras/rep_tracker/trigger.lua
+++ weakauras/rep_tracker/trigger.lua
@@ -1,148 +1,127 @@
 function(allstates)
     -- Generic Variables
-    local anyFound = false
     local general = aura_env.config.general 
+    local colours = aura_env.config.colourOptions
     
-    -- Current Expansion
-    local currentExpansion = aura_env.config.currentExpansion
-    
-    -- C_MajorFactions API
-    local GetRenownLevels = C_MajorFactions.GetRenownLevels
-    local GetMajorFactionData = C_MajorFactions.GetMajorFactionData
-    local HasMaximumRenown = C_MajorFactions.HasMaximumRenown
-    
-    ------------------------------------------------------------------------------
-    
-    for factionID, isTracked in pairs(currentExpansion) do 
-        
-        if isTracked then 
-            -- HasMaximumRenown
-            local isMaxRenown = HasMaximumRenown(factionID)
-            -- GetMajorFactionData
-            local factionData = GetMajorFactionData(factionID)
-            
-            -- Renown
-            if factionData and
-            (general.showRenownProg or general.showRenownLevel) 
-            and not isMaxRenown or (isMaxRenown and not general.hideAtMaxRenown)
-            then 
-                -- We have atleast found 1 to show
-                anyFound = true
+    for _, expansion in ipairs(aura_env.expansions) do
+        for factionID, isTracked in pairs(aura_env.config[expansion]) do
+            if isTracked then 
                 
-                local repName = factionData["name"]
-                local renownLevel = factionData["renownLevel"]
-                local renownReputationEarned = factionData["renownReputationEarned"]
-                local renownLevelThreshold = factionData["renownLevelThreshold"]
+                -- Renown
+                local isMaxRenown = C_MajorFactions.HasMaximumRenown(factionID)
+                local factionData = C_MajorFactions.GetMajorFactionData(factionID)
+                -- Friendship
+                local friendship = C_GossipInfo.GetFriendshipReputation(factionID)
                 
-                -- GetRenownLevels
-                local allRenownLevels = GetRenownLevels(factionID)
-                local maxRenownLevelData = allRenownLevels[#allRenownLevels]
-                local maxRenownLevel = maxRenownLevelData["level"]
-                
-                --Should given renown faction be shown
-                local showRenownFaction = renownLevel > 0 or renownReputationEarned > 0 or general.showUnmetRenownFaction
-                
-                if showRenownFaction then
+                --------------------- Check if it's a renown faction ---------------------
+                if factionData and
+                (general.showRenownProg or general.showRenownLevel) 
+                and not isMaxRenown or (isMaxRenown and not general.hideAtMaxRenown)
+                then 
+                    local repName = factionData["name"]
+                    local renownLevel = factionData["renownLevel"]
+                    local renownReputationEarned = factionData["renownReputationEarned"]
+                    local renownLevelThreshold = factionData["renownLevelThreshold"]
                     
-                    local curVal, threshold, _, hasRewardPending = C_Reputation.GetFactionParagonInfo(factionID)
+                    -- GetRenownLevels
+                    local allRenownLevels = C_MajorFactions.GetRenownLevels(factionID)
+                    local maxRenownLevelData = allRenownLevels[#allRenownLevels]
+                    local maxRenownLevel = maxRenownLevelData["level"]
+                    
+                    --Should given renown faction be shown
+                    local showRenownFaction = (renownLevel > 0 or renownReputationEarned > 0 or general.showUnmetRenownFaction) and (not general.onlyShowMaxRenownIfParagonBox or hasRewardPending or not isMaxRenown)
+                    
+                    allstates[factionID] = {
+                        changed = true,
+                        show = showRenownFaction,
+                        progressType = "static",
+                        repValue = aura_env.getRenownColouredValue(renownReputationEarned, renownLevelThreshold, renownLevel, maxRenownLevel, isMaxRenown, factionID),
+                        icon = aura_env.reputations[factionID],
+                        name = aura_env.getColouredRepName(repName)
+                    }
+                    --------------------- Check if it's a friendship reputation ---------------------
+                    --friendship.nextThreshold is nil when a friendship is exalted
+                elseif friendship and friendship.maxRep > 0 and friendship.nextThreshold then
+                    local barMin = friendship.reactionThreshold
+                    local barMax = friendship.nextThreshold or friendship.standing 
+                    local barValue = friendship.standing
                     
                     allstates[factionID] = {
                         changed = true,
                         show = true,
                         progressType = "static",
-                        value = aura_env.getRenownColouredValue(renownReputationEarned, renownLevelThreshold, renownLevel, maxRenownLevel, isMaxRenown, factionID),
+                        repValue = aura_env.getColouredValue(barMin, barMax, barValue, friendship.reaction),
                         icon = aura_env.reputations[factionID],
-                        name = aura_env.getColouredRepName(repName)
+                        name = aura_env.getColouredRepName(friendship.name)
                     }
-                end
-            end
-            
-            -- Reputation
-            if not factionData then
-                local repName, _, standingId, barMin, barMax, barValue = GetFactionInfoByID(factionID)
-                
-                local isAssumedUnmet = standingId == 4 and barMin == 0
-                
-                if barValue and 
-                (barValue > 0 or general.showHostile and barValue < 0 or general.showUnmet and isAssumedUnmet) and 
-                barValue < 42000 and 
-                general.showBeforeExalted
-                then
+                    --------------------- Assume it's a regular reputation ---------------------
+                else
+                    local fd = C_Reputation.GetFactionDataByID(factionID)
                     
-                    -- We have atleast found 1 to show
-                    anyFound = true
+                    local repName = fd.name
+                    local standingId = fd.reaction
+                    local barMin = fd.currentReactionThreshold
+                    local barMax = fd.nextReactionThreshold
+                    local barValue = fd.currentStanding
                     
-                    allstates[factionID] = {
-                        changed = true,
-                        show = true,
-                        progressType = "static",
-                        value = aura_env.getColouredValue(barMin, barMax, barValue, standingId),
-                        icon = aura_env.reputations[factionID],
-                        name = aura_env.getColouredRepName(repName)
-                    }
-                end
-                
-            end
-        end
-    end
-    
-    -- Previous Expansions
-    local previousExpansions = aura_env.config.previousExpansions
-    
-    for xpacKey, _ in pairs(previousExpansions) do
-        for factionID, isTracked in pairs(previousExpansions[xpacKey]) do
-            if isTracked then
-                
-                local repName, _, standingId, barMin, barMax, barValue = GetFactionInfoByID(factionID)
-                
-                local isAssumedUnmet = standingId == 4 and barMin == 0
-                
-                if barValue and 
-                (barValue > 0 or general.showHostile and barValue < 0 or general.showUnmet and isAssumedUnmet) and 
-                barValue < 42000 and 
-                general.showBeforeExalted
-                then
+                    local isAssumedUnmet = standingId == 4 and barMin == 0
                     
-                    -- We have atleast found 1 to show
-                    anyFound = true
+                    --check if faction has paragon levels
+                    local paragonValue = C_Reputation.GetFactionParagonInfo(factionID)
                     
-                    -- Set REPUTATION information for the faction
-                    allstates[factionID] = {
-                        changed = true,
-                        show = true,
-                        progressType = "static",
-                        value = aura_env.getColouredValue(barMin, barMax, barValue, standingId),
-                        icon = aura_env.reputations[factionID],
-                        name = aura_env.getColouredRepName(repName)
-                    }
-                    
-                    --Faction is exalted
-                elseif general.showAfterExalt then
-                    
-                    local curVal, threshold, _, hasRewardPending = C_Reputation.GetFactionParagonInfo(factionID)
-                    
-                    --Faction has paragon rewards possible
-                    if curVal and threshold and (not general.onlyShowParagonRewards or hasRewardPending) then 
-                        
-                        -- We have atleast found 1 to show
-                        anyFound = true
-                        
-                        -- Set PARAGON information for the faction
+                    if barValue and 
+                    (
+                        barValue > 0 or 
+                        general.showHostile and barValue < 0 or 
+                        general.showUnmet and isAssumedUnmet
+                    ) and 
+                    (
+                        barValue < barMax and general.showBeforeExalted or 
+                        barValue == barMax and general.showAfterExalted and not paragonValue
+                    )
+                    then
+                        -- Set reputation information for the faction
                         allstates[factionID] = {
                             changed = true,
                             show = true,
                             progressType = "static",
-                            value = aura_env.getParagonColouredValue(curVal, threshold, hasRewardPending, standingId),
+                            repValue = aura_env.getColouredValue(barMin, barMax, barValue, standingId),
                             icon = aura_env.reputations[factionID],
                             name = aura_env.getColouredRepName(repName)
                         }
+                        
+                        --Faction is exalted
+                    elseif general.showAfterExalted then
+                        
+                        local curVal, threshold, _, hasRewardPending = C_Reputation.GetFactionParagonInfo(factionID)
+                        
+                        --Faction has paragon rewards possible
+                        if curVal and threshold and (not general.onlyShowParagonRewards or hasRewardPending) then 
+                            -- Set PARAGON information for the faction
+                            allstates[factionID] = {
+                                changed = true,
+                                show = true,
+                                progressType = "static",
+                                repValue = aura_env.getParagonColouredValue(curVal, threshold, hasRewardPending, standingId),
+                                icon = aura_env.reputations[factionID],
+                                name = aura_env.getColouredRepName(repName)
+                            }
+                        end
                     end
                 end
             end
         end
     end
+    
+    local count = 0
+    for k in pairs(allstates) do
+        if allstates[k].show then
+            count = count + 1
+            break
+        end 
+    end
     -- if non found then show nothing
-    if not anyFound then 
+    if count == 0 then 
         allstates[""] = {
             changed = true,
             show = true,
@@ -153,5 +132,4 @@ function(allstates)
     
     return true
 end
-
 
