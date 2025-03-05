d362699101eeb3670f0fd4d01c81d99d572b289a
Pewtro <29204244+Pewtro@users.noreply.github.com>
2025-03-05 14:14:33 +0100

    [Crest] Update costs of gilded

--- src/weakauras/crest_tracker/on_init.lua
+++ src/weakauras/crest_tracker/on_init.lua
@@ -6,7 +6,7 @@ aura_env.table = {
     --Weathered
     ["rank1"] = { 
         currency  = { id = 3107, index = 2 }, --Weathered Undermine Crest
-        item = { cost = 30, id = 230937, index = 6 }, --Nascent Weathered Undermine Crest
+        item = { cost = 30, id = 230937, index = 6 }, --Enchanted Weathered Undermine Crest
         achievement = 40942, --Weathered of the Undermine
     },
     --Carved
@@ -17,13 +17,13 @@ aura_env.table = {
     --Runed
     ["rank3"] = {
         currency  = { id = 3109, index = 4 }, --Runed Undermine Crest
-        item = { cost = 45, id = 230936, index = 7 }, --Nascent Runed Undermine Crest
+        item = { cost = 45, id = 230936, index = 7 }, --Enchanted Runed Undermine Crest
         achievement = 40944, --Runed of the Undermine
     },
     --Gilded
     ["rank4"] = {
         currency  = { id = 3110, index = 5 }, --Gilded Undermine Crest
-        item = { cost = 90, id = 230935, index = 8 }, --Nascent Gilded Undermine Crest
+        item = { cost = 60, id = 230935, index = 8 }, --Enchanted Gilded Undermine Crest
         achievement = 40945, --Gilded of the Undermine
     },
 }
